2e0abb263940c8b07d34c85db4f5fda421d36198
Bendimester23 <58264102+Bendimester23@users.noreply.github.com>
2021-02-03 14:36:22 +0100

    Initial Commit

---
+++ src/main/java/hu/bendi/skylib/Skylib.java
@@ -0,0 +1,10 @@
+package hu.bendi.skylib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Skylib implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/hu/bendi/skylib/client/SkylibClient.java
@@ -0,0 +1,13 @@
+package hu.bendi.skylib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SkylibClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
